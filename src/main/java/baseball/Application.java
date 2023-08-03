702c7c46a36e40a06a51992108a4f1db77d16bfc
Gwak-daeun <bbibibbibi22@gmail.com>
2023-08-03 23:23:44 +0900

    feat(Application): 유저가 잘못된 값 입력 시 예외처리 메소드 완성. 컴퓨터와 유저의 숫자 비교하는 메소드 만드는 중

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,17 +6,22 @@ import java.util.Set;
 
 public class Application {
 
-    public static boolean isInteger(String userInputNum) {
+    public String compareComAndUser(Set<Integer> com, String user) {
+    //user 문자열을 배열로 나눠 com과 비교해야
+
+    }
+
+    public boolean isInteger(String userInputNum) {
 
         char temp;
 
-        boolean isNum = true;
+        boolean isNum = false;
 
         for (int word = 0; word < userInputNum.length(); word++) {
             temp = userInputNum.charAt(word);
 
             if (Character.isDigit(temp) == false) {
-                isNum = false;
+                isNum = true;
             }
         }
         System.out.println(isNum);
@@ -35,6 +40,8 @@ public class Application {
                 throw new IllegalArgumentException("잘못입력하셨습니다. 숫자 3자리를 입력해주세요.");
             }
 
+            return userInputNum;
+
         } catch (IllegalArgumentException illegalArgumentException) {
             System.out.println(illegalArgumentException.getMessage());
             System.exit(0);
@@ -43,7 +50,7 @@ public class Application {
         return "";
     }
 
-    public Set<Integer> computerNumber() { // 컴퓨터가 가지고 있을 숫자
+    public Set<Integer> computerNumberMaker() { // 컴퓨터가 가지고 있을 숫자
         Set<Integer> comNums = new HashSet<>(); // 중복을 허용하지 않는 Set
 
         while (comNums.size() != 3) {
@@ -58,11 +65,17 @@ public class Application {
     public void gameAnnounce() { // 게임 안내 문구
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        Set<Integer> comNums = computerNumber();
+        Set<Integer> comNums = computerNumberMaker();
+
+
 
         System.out.println("숫자를 입력해주세요 : ");
 
-        userNumberInput();
+        String userNum = userNumberInput();
+
+        //컴퓨터의 숫자와 유저의 숫자를 비교하는 메소드
+        compareComAndUser(comNums, userNum);
+
 
     }
 
