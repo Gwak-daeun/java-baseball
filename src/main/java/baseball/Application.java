0fcc96a81e8cdd5b9065c6d78fd28018c4539977
Gwak-daeun <bbibibbibi22@gmail.com>
2023-08-06 01:01:47 +0900

    feat(Application): 사용자가 숫자를 맞출 때까지 게임 종료되지 않는 메소드 완성. 맞춘 후 게임을 종료할지 재시작 할지 고르는 메소드 만드는 중

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,54 +4,104 @@ import java.util.*;
 
 public class Application {
 
+    List<Integer> userNum = new ArrayList<>();
 
-    public Map<String, Integer> compareComAndUser(Set<Integer> com, String userInputString) { // 스트라이크, 볼, 낫싱
+    boolean isComAndUserSame = true;
+
+    Scanner userInput = new Scanner(System.in);
+
+
+    public Map<String, Integer> insertStrikeAndBallAndNothing(Set<Integer> com) { // 스트라이크, 볼, 낫싱
         //user 문자열을 배열로 나눠 com과 비교해야
-
-        List<Integer> userNum = changeUserStringIntoNum(userInputString);
 
         List<Integer> comNum = changeComSetIntoNum(com);
 
-//        List<String> resultWord = new ArrayList<>();
-
         Map<String, Integer> resultWord = new HashMap<>();
 
-        resultWord.put("스트라이크", 0);
-        resultWord.put("볼", 0);
-        resultWord.put("낫싱", 0);
+        while (isComAndUserSame){
+            System.out.println("숫자를 입력해주세요 : ");
 
-        int strikeCheck = 0;
-        int ballCheck = 0;
-        int nothingCheck = 3;
+            String userInputString = userInput.nextLine();
 
-        boolean strikeChecked = true;
+            checkUserNumber(userInputString);
 
-        for (int userNumIdx = 0; userNumIdx < userNum.size(); userNumIdx++) {
-            for (int comIdx = 0; comIdx < comNum.size(); comIdx++) {
-                if (comNum.get(comIdx) == userNum.get(userNumIdx)) {
-                    if (comIdx == userNumIdx) { // 스트라이크인 경우
-                        strikeCheck++;
-                        strikeChecked = false;
-                    }
-                    if (comIdx != userNumIdx && strikeChecked) { // 볼인 경우
-                        if (strikeCheck == comIdx + 1) {
-                            continue;
-                        } else {
-                            ballCheck++;
+            userNum = changeUserStringIntoNum(userInputString);
+
+            isComAndUserSame = compareComAndUser(comNum, userNum);
+
+//        List<String> resultWord = new ArrayList<>();
+
+            resultWord.put("스트라이크", 0);
+            resultWord.put("볼", 0);
+            resultWord.put("낫싱", 0);
+
+            int strikeCheck = 0;
+            int ballCheck = 0;
+            int nothingCheck = 3;
+
+            boolean strikeChecked = true;
+
+            for (int userNumIdx = 0; userNumIdx < userNum.size(); userNumIdx++) {
+                for (int comIdx = 0; comIdx < comNum.size(); comIdx++) {
+                    if (comNum.get(comIdx) == userNum.get(userNumIdx)) {
+                        if (comIdx == userNumIdx) { // 스트라이크인 경우
+                            strikeCheck++;
+                            strikeChecked = false;
                         }
+                        if (comIdx != userNumIdx && strikeChecked) { // 볼인 경우
+                            if (strikeCheck == comIdx + 1) {
+                                continue;
+                            } else {
+                                ballCheck++;
+                            }
+                        }
+                        strikeChecked = true;
                     }
-                    strikeChecked = true;
                 }
+            }
+
+            nothingCheck -= strikeCheck + ballCheck;
+
+            resultWord.replace("스트라이크", strikeCheck);
+            resultWord.replace("볼", ballCheck);
+            resultWord.replace("낫싱", nothingCheck);
+
+            if (resultWord.get("스트라이크") != 0) {
+                System.out.print(resultWord.get("스트라이크") + "스트라이크 ");
+            }
+            if (resultWord.get("볼") != 0) {
+                System.out.print(resultWord.get("볼") + "볼 ");
+            }
+            if (resultWord.get("낫싱") != 0) {
+                System.out.print(resultWord.get("낫싱") + "낫싱 " + "\n");
             }
         }
 
-        nothingCheck -= strikeCheck + ballCheck;
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
-        resultWord.replace("스트라이크", strikeCheck);
-        resultWord.replace("볼", ballCheck);
-        resultWord.replace("낫싱", nothingCheck);
+        choiceRestartOrEndGame();
+
 
         return resultWord;
+    }
+
+    public void choiceRestartOrEndGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        
+
+    }
+
+    public boolean compareComAndUser(List<Integer> comNum, List<Integer> userNum) {
+
+        boolean isNotSame = false;
+
+        for (int i = 0; i < comNum.size(); i++) {
+            if (!Objects.equals(comNum.get(i), userNum.get(i))) {
+                isNotSame = true;
+            }
+        }
+
+        return isNotSame;
     }
 
     public List<Integer> changeComSetIntoNum(Set<Integer> com) {
@@ -71,8 +121,8 @@ public class Application {
 
         List<Integer> userNum = new ArrayList<>();
 
-        for (int un = 0; un < userInputString.length(); un++) {
-            char userInt = userInputString.charAt(un);
+        for (int userNumIdx = 0; userNumIdx < userInputString.length(); userNumIdx++) {
+            char userInt = userInputString.charAt(userNumIdx);
             userNum.add(Character.getNumericValue(userInt));
         }
         System.out.println(userNum);
@@ -96,13 +146,9 @@ public class Application {
         return isNum;
     }
 
-    public String insertUserNumber() { // 사용자가 입력한 숫자를 받는다.(숫자 입력했는지 확인 후 예외처리)
-
-        Scanner userInput = new Scanner(System.in);
+    public String checkUserNumber(String userInputString) { // 사용자가 입력한 숫자를 받는다.(숫자 입력했는지 확인 후 예외처리)
 
         try {
-
-            String userInputString = userInput.nextLine();
 
             if (userInputString.length() != 3 || isInteger(userInputString)) {
                 throw new IllegalArgumentException("잘못입력하셨습니다. 숫자 3자리를 입력해주세요.");
@@ -118,7 +164,7 @@ public class Application {
         return "";
     }
 
-    public Set<Integer> MakeComputerNumber() { // 컴퓨터가 가지고 있을 숫자
+    public Set<Integer> makeComputerNumber() { // 컴퓨터가 가지고 있을 숫자
         Set<Integer> comNums = new HashSet<>(); // 중복을 허용하지 않는 Set
 
         while (comNums.size() != 3) {
@@ -133,17 +179,16 @@ public class Application {
     public void announceGame() { // 게임 안내 문구
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        Set<Integer> comNums = MakeComputerNumber();
+        Set<Integer> comNums = makeComputerNumber();
 
+//        System.out.println("숫자를 입력해주세요 : ");
 
-        System.out.println("숫자를 입력해주세요 : ");
-
-        String userInputString = insertUserNumber();
+//        String userInputString = checkUserNumber();
 
         //컴퓨터의 숫자와 유저의 숫자를 비교하는 메소드
-        Map<String, Integer> resultWord = compareComAndUser(comNums, userInputString);
+        Map<String, Integer> resultWord = insertStrikeAndBallAndNothing(comNums);
 
-        System.out.println(resultWord);
+
     }
 
     public static void main(String[] args) {
